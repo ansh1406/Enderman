d93bc3e59bbc54ae166fe5c8f14a10232c6cdc25
ansh1406 <anshswaroop14@gmail.com>
2026-02-02 21:23:40 +0530

    Enderman class : Main class for creating web app , and functions to register middleware

---
+++ includes/enderman/enderman.hpp
@@ -0,0 +1,27 @@
+#ifndef ENDERMAN_HPP
+#define ENDERMAN_HPP
+
+#include "enderman/types.hpp"
+
+#include <functional>
+#include <stdexcept>
+#include <string>
+
+namespace enderman
+{
+    class Enderman
+    {
+    private:
+        struct Impl;
+        Impl *pImpl = nullptr;
+
+    public:
+        Enderman();
+
+        void use(const std::vector<std::string> paths, MiddlewareFunction func);
+        void use(const std::string &path, MiddlewareFunction func);
+        void use(MiddlewareFunction func);
+    };
+}
+
+#endif // ENDERMAN_HPP
---
+++ src/enderman.cpp
@@ -0,0 +1,41 @@
+#include "enderman/types.hpp"
+#include "enderman/enderman.hpp"
+#include "enderman/utils.hpp"
+#include "enderman/middleware.hpp"
+#include "enderman/route_handler.hpp"
+
+#include <functional>
+#include <stdexcept>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+
+namespace enderman
+{
+    struct Enderman::Impl
+    {
+        std::vector<Middleware> middlewares;
+        std::unordered_map<std::string, std::vector<RouteHandler>> routes;
+    };
+}
+
+enderman::Enderman::Enderman() : pImpl(new Impl()) {}
+
+void enderman::Enderman::use(const std::string &path, MiddlewareFunction func)
+{
+    auto segments = enderman::utils::UriParser::parse_path(path);
+    pImpl->middlewares.push_back(Middleware({segments}, std::move(func)));
+}
+
+void enderman::Enderman::use(const std::vector<std::string> paths, MiddlewareFunction func)
+{
+    for (const auto &path : paths)
+    {
+        use(path, func);
+    }
+}
+
+void enderman::Enderman::use(MiddlewareFunction func)
+{
+    pImpl->middlewares.push_back(Middleware({}, std::move(func)));
+}
