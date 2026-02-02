90576546a878dc1df4ebcc20d12c4d9ddffd4d0b
ansh1406 <anshswaroop14@gmail.com>
2026-02-02 21:03:04 +0530

    Added middleware and route handler class files

---
+++ includes/enderman/middleware.hpp
@@ -0,0 +1,27 @@
+#ifndef ENDERMAN_MIDDLEWARE_HPP
+#define ENDERMAN_MIDDLEWARE_HPP
+
+#include "enderman/types.hpp"
+
+#include <functional>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <utility>
+
+namespace enderman
+{
+    class Request;
+    class Response;
+
+    struct Middleware
+    {
+        std::vector<std::string> path;
+        MiddlewareFunction func;
+
+        explicit Middleware(const std::vector<std::string> p, MiddlewareFunction f)
+            : path(std::move(p)), func(std::move(f)) {}
+    };
+}
+
+#endif // ENDERMAN_MIDDLEWARE_HPP
---
+++ includes/enderman/route_handler.hpp
@@ -0,0 +1,27 @@
+#ifndef ENDERMAN_ROUTE_HANDLER_HPP
+#define ENDERMAN_ROUTE_HANDLER_HPP
+
+#include "enderman/types.hpp"
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <utility>
+
+namespace enderman
+{
+    class Request;
+    class Response;
+
+    struct RouteHandler
+    {
+        std::vector<std::string> path;
+        RouteHandlerFunction handler;
+        explicit RouteHandler(const std::vector<std::string> _path, RouteHandlerFunction f)
+            : path(std::move(_path)), handler(std::move(f))
+        {
+        }
+    };
+}
+
+#endif // ENDERMAN_ROUTE_HANDLER_HPP
