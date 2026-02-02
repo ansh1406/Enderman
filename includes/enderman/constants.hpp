385069f435c7b85b7e1f1391c55a05df0a7f2478
ansh1406 <anshswaroop14@gmail.com>
2026-02-02 21:36:29 +0530

    Added enum for Http Methods

--- includes/enderman/constants.hpp
+++ includes/enderman/constants.hpp
@@ -1,4 +1,19 @@
 #ifndef ENDERMAN_CONSTANTS_HPP
 #define ENDERMAN_CONSTANTS_HPP
 
+namespace enderman
+{
+    enum class HttpMethod
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        PATCH,
+        OPTIONS,
+        HEAD,
+
+    };
+}
+
 #endif // ENDERMAN_CONSTANTS_HPP
