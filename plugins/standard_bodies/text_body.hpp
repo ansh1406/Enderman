dc62c4f277e66ea0d902e5805344b7b3f3205ae4
ansh1406 <anshswaroop14@gmail.com>
2026-02-06 18:05:00 +0530

    added text body parser

--- plugins/standard_bodies/text_body.hpp
+++ plugins/standard_bodies/text_body.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include "enderman/request.hpp"
+#include "enderman/response.hpp"
+#include "enderman/types.hpp"
 #include "enderman/body.hpp"
 
 #include <string>
+#include <cctype>
+#include <unordered_set>
+#include <memory>
 
 namespace enderman
 {
+    std::unordered_set<std::string> text_types = {
+        "text/plain",
+        "text/html",
+        "text/css",
+        "text/javascript",
+        "text/csv",
+        "text/xml",
+        "text/markdown",
+        "application/xml",
+        "application/xhtml+xml",
+        "application/javascript",
+        "application/json",
+    };
+
     class TextBody : public Body
     {
     public:
@@ -14,7 +34,7 @@ namespace enderman
 
         TextBody(const std::string &type = "text/plain") : type(type) {}
 
-        void parse_from(const std::vector<char> &body) override
+        void parse_from(const std::vector<char> &body)
         {
             text = std::string(body.begin(), body.end());
         }
@@ -29,4 +49,40 @@ namespace enderman
             return type;
         }
     };
+
+    MiddlewareFunction text_body_parser = MiddlewareFunction(
+        [](Request &req, Response &res, Next next)
+        {
+            auto content_type_it = req.headers().find("content-type");
+            if (content_type_it != req.headers().end())
+            {
+                std::string content_type = content_type_it->second;
+                for (auto &c : content_type)
+                    c = std::tolower(c);
+                if (text_types.find(content_type) != text_types.end())
+                {
+                    std::shared_ptr<TextBody> text_body = std::make_shared<TextBody>(content_type);
+                    text_body->parse_from(req.get_body()->as<RawBody>()->data);
+                    req.set_body(text_body);
+                }
+            }
+            next(nullptr);
+        });
+
+    MiddlewareFunction text_body_parser_forced_parsing = MiddlewareFunction(
+        [](Request &req, Response &res, Next next)
+        {
+            auto content_type_it = req.headers().find("content-type");
+            std::string content_type = "text/plain";
+            if (content_type_it != req.headers().end())
+            {
+                content_type = content_type_it->second;
+                for (auto &c : content_type)
+                    c = std::tolower(c);
+            }
+            std::shared_ptr<TextBody> text_body = std::make_shared<TextBody>(content_type);
+            text_body->parse_from(req.get_body()->as<RawBody>()->data);
+            req.set_body(text_body);
+            next(nullptr);
+        });
 }
