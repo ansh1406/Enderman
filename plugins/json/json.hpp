9cda3ee8320e0e1be19d5cc9722214af90e329c5
ansh1406 <anshswaroop14@gmail.com>
2026-02-07 14:54:24 +0530

    created single include and include guard

--- plugins/json/json.hpp
+++ plugins/json/json.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "json_core.hpp"
 #include "json_body.hpp"
 #include "json_parser.hpp"
---
+++ plugins/standard_bodies/standard_bodies.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "formdata_body.hpp"
+#include "text_body.hpp"
