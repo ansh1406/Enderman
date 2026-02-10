f533dc719ed9f9d3b14705dc70a687eff404f7b3
ansh1406 <anshswaroop14@gmail.com>
2026-02-10 16:56:21 +0530

    Added more QoL funcions in JsonBody

--- plugins/json/include/enderman/json/json_body.hpp
+++ plugins/json/include/enderman/json/json_body.hpp
@@ -4,6 +4,10 @@
 #pragma once
 #include "json_core.hpp"
 #include "enderman/body.hpp"
+#include "enderman/request.hpp"
+#include "enderman/response.hpp"
+
+#include <memory>
 
 namespace enderman
 {
@@ -12,17 +16,37 @@ namespace enderman
     {
     public:
         enderman_json::Object jsonobj;
+
         JsonBody(enderman_json::Object obj = enderman_json::Object()) : jsonobj(obj) {}
+
         void parse_from(const std::vector<char> &body)
         {
             std::string str(body.begin(), body.end());
-            jsonobj = enderman_json::parse_json(str);
+            parse_from(str);
         }
+
+        void parse_from(const std::string &body)
+        {
+            jsonobj = enderman_json::parse_json(body);
+        }
+
         std::vector<char> serialize() const override
         {
             std::string str = enderman_json::stringify(jsonobj);
             return std::vector<char>(str.begin(), str.end());
         }
+
         const std::string type() const override { return std::string("application/json"); }
+
+        /// @brief Creates a new JsonBody with the given JSON object and sets it as the body of the response.
+        /// @param res The response object to set the body on.
+        /// @param obj The JSON object to set in the body.
+        /// @return Reference to the response object with the new body set.
+        static Response &set_json(Response &res, enderman_json::Object &obj)
+        {
+            auto body = std::make_shared<JsonBody>(obj);
+            res.set_body(body);
+            return res;
+        }
     };
 }
