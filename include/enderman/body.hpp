06578169ca5f83c20ecf70eaae9c382ce060420a
ansh1406 <anshswaroop14@gmail.com>
2026-03-03 19:10:23 +0530

    Added docummentation for Body and RawBody class

--- include/enderman/body.hpp
+++ include/enderman/body.hpp
@@ -10,14 +10,25 @@
 
 namespace enderman
 {
+    /// @brief Abstract base class representing the body of a Request or Response.
     class Body
     {
     public:
         Body() = default;
         virtual ~Body() = default;
+
+        /// @brief Serializes the body into a vector of characters.
+        /// @return The serialized body as a vector of characters.
         virtual std::vector<char> serialize() const = 0;
+        /// @brief Content type of the body, e.g. "application/json", "text/plain", etc.
+        /// This will be used to set the Content-Type header of the message.
+        /// @return Content type as a string.
         virtual const std::string type() const = 0;
 
+        /// @brief Casts body to a specific type T.
+        /// @tparam T Type to cast to, must be derived from Body.
+        /// @return Object of type T if the cast is successful.
+        /// @throws std::bad_cast if the cast fails.
         template <typename T>
         const T *as() const
         {
@@ -28,7 +39,10 @@ namespace enderman
             }
             return casted;
         }
-
+        /// @brief Casts body to a specific type T.
+        /// @tparam T Type to cast to, must be derived from Body.
+        /// @return Object of type T if the cast is successful.
+        /// @throws std::bad_cast if the cast fails.
         template <typename T>
         T *as()
         {
@@ -41,20 +55,29 @@ namespace enderman
         }
     };
 
+    /// @brief A simple implementation of Body that holds raw binary data as a vector of characters.
     class RawBody : public Body
     {
     public:
+        /// @brief The raw binary data of the body.
         std::vector<char> data;
+
         RawBody() = default;
         ~RawBody() override = default;
+        /// @brief Set data in body from a vector of characters.
+        /// @param body The vector of characters to set as the body data.
         void parse_from(const std::vector<char> &body)
         {
             data = body;
         }
+        /// @brief Serializes the raw body data into vector of characters.
+        /// @return The raw body data as a vector of characters.
         std::vector<char> serialize() const override
         {
             return data;
         }
+        /// @brief Get type of RawBody.
+        /// @return application/octet-stream
         const std::string type() const override { return std::string("application/octet-stream"); }
     };
 }
